fd11ab4f18d94cd6d9692955a7bed91d8cb166fa
danielgp <danielgarduno282@gmail.com>
2025-09-05 13:07:36 -0600

    Se resolvieron los primeros 5 tasks

--- task4.js
+++ task4.js
@@ -1,7 +1,18 @@
 export class Player {
-    constructor() {
-      
+  name;
+  level;
+  constructor(name, level) {
+      this.name=name;
+      this.level=level;
     }
-  
-    
+  info(){
+    return `${this.name} has reached level ${this.level}`
   }
+  levelup(){
+    this.level++;
+    return `El nuevo nivel de ${this.name} es ${this.level}`
+  }
+  }
+const player1 = new Player("Tara",6);
+console.log(player1.info());
+console.log(player1.levelup());
